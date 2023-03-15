c998a3e97f0d8ac8880676737b17a0aee45cd6c0
KlimovDima <klimov-dima-99@yandex.ru>
2023-03-15 23:22:46 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -10,57 +10,109 @@ public class Main {
         task8();
         task9();
         task10();
+        // year();
 
     }
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Задача 1");
-        // Пишем код для задачи 1
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(i);
+        }
     }
 
-    public static void task2 () {
+    public static void task2() {
         System.out.println("Задача 2");
-        // Пишем код для задачи 2
+        for (int i = 10; i > 0; i = i - 1) {
+            System.out.println(i);
+        }
     }
 
-    public static void task3 () {
+    public static void task3() {
         System.out.println("Задача 3");
-        // Пишем код для задачи 2
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
     }
 
-    public static void task4 () {
+    public static void task4() {
         System.out.println("Задача 4");
-        // Пишем код для задачи 2
+        for (int i = 10; i >= -10; i = i - 1) {
+            System.out.println(i);
+        }
     }
 
-    public static void task5 () {
+    /*
+     public static void year() {
+        for (int i = 2020; i < 2070; i = i + 4) {
+            System.out.println("Високосный год " + i);
+        }
+     }
+    */
+
+    public static void task5() {
         System.out.println("Задача 5");
-        // Пишем код для задачи 2
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " является високосным");
+        }
     }
 
-    public static void task6 () {
+    public static void task6() {
         System.out.println("Задача 6");
-        // Пишем код для задачи 2
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
     }
 
-    public static void task7 () {
+    public static void task7() {
         System.out.println("Задача 7");
-        // Пишем код для задачи 2
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
     }
 
-    public static void task8 () {
+    /*
+       public static void task8() {
         System.out.println("Задача 8");
-        // Пишем код для задачи 2
+        int salary = 65635;
+        int total = 0;
+       for (int i = 0; i <12; i++){
+            total = total + total/100;
+            total=total+salary;
+            System.out.println("Месяц " + i + " Итого:" + total);
+        }
+        System.out.println(total);
+    }
+    */
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int mothReplenishment = 29_000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++){
+            total=total+mothReplenishment;
+            System.out.println("Месяц " + i + " Сумма накоплений равна:" + total + " рублей");
+        }
     }
 
-    public static void task9 () {
+    public static void task9() {
         System.out.println("Задача 9");
-        // Пишем код для задачи 2
+        int mothReplenishment = 29_000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++){
+            total=total + total/100;
+            total=total+mothReplenishment;
+            System.out.println("Месяц " + i + " Сумма накоплений равна:" + total + " рублей");
+        }
     }
 
-    public static void task10 () {
+    public static void task10() {
         System.out.println("Задача 10");
-        // Пишем код для задачи 10
+        int result;
+        for (int i=1;i<=10;i++){
+            result=2*i;
+            System.out.println("2 * " + i + " = " + result);
+        }
     }
 
 }
